5c75e41b3e3ba9c62f877706da9ff5ff08359409
sara <saraatiyabi18@gmail.com>
2025-07-25 11:51:00 +0330

    init

---
+++ app/components/BaseNavigation.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+      <NuxtLink to="/" class="flex gap-1 items-center">
+        <NuxtImg
+          src="/icon-green.png"
+          sizes="30px sm:35px md:45px"
+          densities="x1"
+          format="webp"
+        />
+        <span class="text-3xl font-bold">Nuxtcipes</span>
+      </NuxtLink>
+      <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About Us</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ app/layouts/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1>Login layout</h1>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
